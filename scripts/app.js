757d4f28612eb28ca756a49cf9f060666b1bb155
Chris Brett <chris.brett@citizensadvice.org.uk>
2019-02-15 15:12:45 +0000

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+const App = (() => {
+  const render = () => {
+    console.log("Hello World!");
+    // write your code JS in here...
+  };
+
+  return {
+    render: render,
+  };
+})();
+
+App.render();
